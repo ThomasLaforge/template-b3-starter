c18741d816d3376803cf84f496efcd11020cac76
ThomasLaforge <thomas.laforge.38@gmail.com>
2025-01-22 10:04:44 +0100

    add model typing

--- src/model/Voiture.ts
+++ src/model/Voiture.ts
@@ -1,7 +1,20 @@
-import { DataTypes, Sequelize } from "sequelize";
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+// Define type in TypeScript and add it to the Sequelize instance
+
+interface VoitureAttributes {
+    name: string;
+    immatriculation_date: number;
+}
+
+export interface VoitureInstance extends VoitureAttributes {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
 export const VoitureModel = (sequelize: Sequelize) => {
-    return sequelize.define('voiture', {
+    return sequelize.define<Model<VoitureAttributes, VoitureAttributes>>('voiture', {
         name: DataTypes.STRING,
         immatriculation_date: DataTypes.NUMBER,
     });
